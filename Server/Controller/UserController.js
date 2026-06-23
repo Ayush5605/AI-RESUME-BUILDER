7158538a442b51a98b78e8a907dcc6bb6c9e28a2
Ayush Auti <ayushauti5605@gmail.com>
2026-06-23 18:53:09 +0530

    done with user signup function

---
+++ Server/Controller/UserController.js
@@ -0,0 +1,49 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+import User from "../Model/User.js";
+import bcrypt from "bcryptjs";
+
+const generateToken=(userId)=>{
+    const token=JsonWebTokenError.toString({userId},process.env.JWT_SECRET,{expiresIn:"7d"});
+    return token;
+}
+
+//USER REGISTRATION
+
+
+
+//POST:/api/users/register
+export const registerUser=async(req,res)=>{
+
+    try{
+        const{name,email,user}=req.body;
+
+        if(!name || !email || !password){
+            return res.status(400).json({message:"Missing the required fields"});
+        }
+
+        const user=await User.findOne({email})
+
+        if(user){
+            return res.status(400).json({message:"User already exists"});
+        }
+
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        const newUser=await User.create({
+            name,email,password:hashedPassword
+        });
+
+        const token=generateToken(newUser._id);
+
+        newUser.password=undefined;
+
+        return res.status(201).json({message:"User created successfully",token,user:newUser});
+
+        
+
+    }catch(error){
+        return res.status(400).json({message:error.message})
+
+    }
+
+}
--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 import express from"express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./Controller/db.js";
+import connectDB from "./Config/db.js";
 
 const app=express();
 app.use(express.json());
