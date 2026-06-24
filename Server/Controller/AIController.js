4ced099c331f6551920621635b5b2f4734b45cf1
Ayush Auti <ayushauti5605@gmail.com>
2026-06-24 19:37:35 +0530

    build some ai controller func

---
+++ Server/Controller/AIController.js
@@ -0,0 +1,108 @@
+
+
+export const enhanceProfessionalSummary=async(req,res)=>{
+    try{
+
+        const {userContent}=req.body;
+
+        if(!userContent){
+            return res.status(400).json({message:"Missing required fields"});
+        }
+
+        const response=await ai.chat.completions.create({
+            model=process.env.GEMINI_MODEL,
+            messages:[
+                {role:"System",content:"You are an expert in resume writing.Your task is to enhance the professional summary of a resume.The summary should be 1-2 sentences also highlighting key skills,experience,and career objectives.Make it compelling and ATS-friendly. and only return text no options or anything else . "},
+                {
+                    role:"user",
+                    content:userContent,
+                }
+            ]
+        })
+
+        const enhancedContent=response.choices[0].message.content;
+
+        return res.status(200).json({enhancedContent})
+
+    }catch(error){
+
+                return res.status(400).json({message:error.message})
+
+
+    }
+}
+
+
+
+export const enhanceJobDescription=async(req,res)=>{
+    try{
+
+        const {userContent}=req.body;
+
+        if(!userContent){
+            return res.status(400).json({message:"Missing required fields"});
+        }
+
+        const response=await ai.chat.completions.create({
+            model=process.env.GEMINI_MODEL,
+            messages:[
+                {role:"System",content:"You are an expert in resume writing.Your task is to enhance the job description of a resume.The job description should be 1-2 sentences also highlighting key responsibilities and achievements.Use action verbs and quantifiable results where possible.Make it ATS-friendly and only return text no options or anything else. "},
+                {
+                    role:"user",
+                    content:userContent,
+                }
+            ]
+        })
+
+        const enhancedContent=response.choices[0].message.content;
+
+        return res.status(200).json({enhancedContent})
+
+    }catch(error){
+
+                return res.status(400).json({message:error.message})
+
+
+    }
+}
+
+
+export const uploadResume=async(req,res)=>{
+    try{
+        const{resumeText,title}=req.body;
+
+        const userId=req.userId;
+
+        if(!resumeText){
+            return res.status(400).json({message:"Missing required fields"})
+        }
+
+        const response=await ai.chat.completions.create({
+            model=process.env.GEMINI_MODEL,
+            messages:[
+                {role:"System",
+                 content:"You are an expert in resume writing.Your task is to enhance the professional summary of a resume.The summary should be 1-2 sentences also highlighting key skills,experience,and career objectives.Make it compelling and ATS-friendly. and only return text no options or anything else . "},
+                {
+                    role:"user",
+                    content:userContent,
+                }
+            ]
+        })
+
+        const enhancedContent=response.choices[0].message.content;
+
+        return res.status(200).json({enhancedContent})
+
+    }catch(error){
+
+                return res.status(400).json({message:error.message})
+
+
+    }
+}
+
+
+
+
+
+
